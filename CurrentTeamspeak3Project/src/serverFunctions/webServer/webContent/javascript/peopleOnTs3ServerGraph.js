7f724a7e8d8009ac24e21fb2d8cd01dd95c08a96
GJohannes <johannes.guenthoer@t-online.de>
2018-11-29 15:25:55 +0100

    -history graph is now replacing current graph when used and vise versa -no longer using outdated .js date picking -only reading history graph data if a different date is selected

--- CurrentTeamspeak3Project/src/serverFunctions/webServer/webContent/javascript/peopleOnTs3ServerGraph.js
+++ CurrentTeamspeak3Project/src/serverFunctions/webServer/webContent/javascript/peopleOnTs3ServerGraph.js
@@ -1,54 +1,102 @@
 // public variable whith the current lineChartData. read from when resizing window. 
 //refreshed/written when ajax call from server is comming back
 var lineChartData;
+var historyChartData;
+var lastCheckedHistoryDate;
 
 $(window).resize(function() {
 	drawChart();
+	if(historyChartData !== undefined){
+		drawHistoryChart();		
+	}
+	
 });
 
 function drawChart() {
-	if (lineChartData === undefined) {
-		return;
-	}
+	// case that the current date should be displayed
+	if($('#datepicker').val() === currentDate()){
+		if (lineChartData === undefined) {
+			return;
+		}
+		
+		for (i = 1; i < lineChartData.length; i++) {
+			lineChartData[i][0] = new Date(lineChartData[i][0]);// make unix time
+			// stamp into date
+		}
+		var time = new Date().getTime();
+		var date = new Date(time);
 
-	for (i = 1; i < lineChartData.length; i++) {
-		lineChartData[i][0] = new Date(lineChartData[i][0]);// make unix time
-		// stamp into date
-	}
-	var time = new Date().getTime();
-	var date = new Date(time);
+		lineChartData[lineChartData.length] = [ date,
+				lineChartData[lineChartData.length - 1][1] ]; // add current date
+		// with last number
+		// of peoples
 
-	lineChartData[lineChartData.length] = [ date,
-			lineChartData[lineChartData.length - 1][1] ]; // add current date
-	// with last number
-	// of peoples
-
-	var data = google.visualization.arrayToDataTable(lineChartData);
-
-	var options = {
-		title : '24h history of the number of people online',
-		// curveType: 'function',
-		vAxis : {
-			title : "Number of people",
-			viewWindow : {
-				min : 0,
-			// max: 32
+		var chartData = google.visualization.arrayToDataTable(lineChartData);
+		
+		var options = {
+				title : '24h history of the number of people online',
+			// curveType: 'function',
+			vAxis : {
+				title : "Number of people",
+				viewWindow : {
+					min : 0,
+				// max: 32
+				},
 			},
-		},
-		legend : {
-			position : 'bottom'
-		},
-	};
+			legend : {
+				position : 'bottom'
+			},
+		};
+		
+	// case that an older/newer date should be displayed
+	} else {
+		for (i = 1; i < historyChartData.length; i++) {
+			historyChartData[i][0] = new Date(historyChartData[i][0]);// make unix time stamp into
+												// date
+		}
 
+		var options = {
+			title : 'All events at the given date',
+			// curveType: 'function',
+			vAxis : {
+				title : "Number of people",
+				viewWindow : {
+					min : 0,
+				// max: 32
+				},
+			},
+			legend : {
+				position : 'bottom'
+			},
+		};
+
+		var chartData = google.visualization.arrayToDataTable(historyChartData);
+	}
+	
 	var chart = new google.visualization.LineChart(document
 			.getElementById('curve_chart'));
 
-	chart.draw(data, options);
+	document.getElementById("curve_chart").classList.add('chart'); // set class so that div is set according to the google library
+	
+	chart.draw(chartData, options);
 }
 
 function refreshLineChart() {
 	setInterval(function() {
-		getLineChartData();
+		if($('#datepicker').val() === currentDate()){
+			// selecting a older date after selecting the current day graph 
+			lastCheckedHistoryDate = "JUST SOME STRING";
+			getLineChartData();			
+		} else {
+			
+			if(lastCheckedHistoryDate === $('#datepicker').val()){
+				// do nothing since history data is already there 
+			// get new data since new date is selected
+			} else {
+				lastCheckedHistoryDate = $('#datepicker').val();
+				getHistoryData();				
+			}
+		}
 	}, 1000);
 }
 
@@ -78,32 +126,8 @@ function getHistoryData() {
 					.getTime(),
 		},
 		success : function(data) {
-			for (i = 1; i < data.length; i++) {
-				data[i][0] = new Date(data[i][0]);// make unix time stamp into
-													// date
-			}
-
-			var options = {
-				title : '24h history of the number of people online',
-				// curveType: 'function',
-				vAxis : {
-					title : "Number of people",
-					viewWindow : {
-						min : 0,
-					// max: 32
-					},
-				},
-				legend : {
-					position : 'bottom'
-				},
-			};
-
-			var chartData = google.visualization.arrayToDataTable(data);
-
-			var chart = new google.visualization.LineChart(document
-					.getElementById('history_chart'));
-
-			chart.draw(chartData, options);
+			historyChartData = data;
+			drawChart();
 		},
 		failure : function(data) {
 			console.log("ERROR!!! getting line chart data");
@@ -111,7 +135,3 @@ function getHistoryData() {
 		}
 	})
 }
-
-$(function() {
-	$("#datepicker").datepicker();
-});
